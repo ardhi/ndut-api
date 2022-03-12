770ad915d9b7bd0d82b732d8194c3d00bb27f3f9
Ardhi Lukianto <ardhi@lukianto.com>
2022-03-12 11:01:32 +0700

    File attachment handler to model

--- ndut/helper/create.js
+++ ndut/helper/create.js
@@ -1,7 +1,13 @@
 const dbCall = require('./db-call')
+const uploadedAsAttachment = require('../../lib/uploaded-as-attachment')
 
 module.exports = async function ({ model, body, opts = {}, filter }) {
+  const { _ } = this.ndut.helper
   const data = await dbCall.call(this, { model, method: 'create', body, filter })
+  if (opts.reqId) {
+    const modelName = _.isString(model) ? model : model.name
+    await uploadedAsAttachment.call(this, modelName, data.id, opts.reqId)
+  }
   return {
     data,
     message: opts.message || 'recordCreated'
--- ndut/helper/remove.js
+++ ndut/helper/remove.js
@@ -1,9 +1,16 @@
 const dbCall = require('./db-call')
 
 module.exports = async function ({ model, params, opts = {}, filter }) {
+  const { _, fs, getConfig } = this.ndut.helper
+  const config = getConfig()
   const oldData = await dbCall.call(this, { model, method: 'findOne', params: { where: params } })
   if (!oldData) throw this.Boom.notFound('recordNotFound', { ndut: 'api' })
   await dbCall.call(this, { model, method: 'remove', params, filter })
+  if (opts.reqId) {
+    const modelName = _.isString(model) ? model : model.name
+    const dir = `${config.dir.data}/attachment/${modelName}/${oldData.id}`
+    await fs.remove(dir)
+  }
   return {
     oldData,
     message: opts.message || 'recordRemoved'
