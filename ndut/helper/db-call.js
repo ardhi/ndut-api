1aea51df9eeb96a47d279940c9509f5e30fc8473
Ardhi Lukianto <ardhi@lukianto.com>
2022-04-18 21:22:50 +0700

    Updates

--- ndut/helper/db-call.js
+++ ndut/helper/db-call.js
@@ -4,6 +4,7 @@ module.exports = async function ({ model, method, params, body, filter, columns,
   const _method = method
   const _params = _.cloneDeep(params)
   method = method === 'count' ? 'find' : method
+  this.ndutApi.hook = this.ndutApi.hook || {}
   if (this.ndutApi.hook[method]) {
     for (const obj of this.ndutApi.hook[method]) {
       if (_.isFunction(obj.before)) await obj.before({ model, params: _params, body, filter })
@@ -16,8 +17,12 @@ module.exports = async function ({ model, method, params, body, filter, columns,
   }
   let result
   if (_method === 'create') {
-    // TODO: if id is a string, result.id is autonumber ???????
-    result = await modelInstance.create(body)
+    // HACK: if id is a string, result.id is autonumber (at least on SQLITE), so force it to use supplied id
+    result = _.cloneDeep(await modelInstance.create(body))
+    const idType = modelInstance.definition.properties.id.type
+    if (typeof idType === 'function' && idType.name === 'String') {
+      if (!_.isEmpty(body.id)) result.id = body.id
+    }
   }
   else result = await modelInstance[_method](_method === 'count' ? (_params.where || {}) : _params, body)
   if (_method === 'count') return result
---
+++ ndutRoute/route/attachment/@model/@id/@column/@file/delete.js
@@ -0,0 +1,15 @@
+module.exports = {
+  handler: async function (request, reply) {
+    const { fs, getConfig, mime } = this.ndut.helper
+    const { getModelByAlias } = this.ndutDb.helper
+    const config = getConfig()
+    const model = await getModelByAlias(request.params.model)
+    request.silentOnError = true // TODO: put on config and attach on preHandler
+    if (!request.user) throw this.Boom.unauthorized()
+    const file = `${config.dir.data}/attachment/${model}/${request.params.id}` +
+      `/${request.params.column}/${request.params.file}`
+    if (!fs.existsSync(file)) throw this.Boom.notFound()
+    await fs.remove(file)
+    reply.send('DELETED')
+  }
+}
