5d5cc6bb1ff64138b35b584066430f64d7136038
Ardhi Lukianto <ardhi@lukianto.com>
2022-04-21 20:52:47 +0700

    Bug fx

--- ndut/helper/update.js
+++ ndut/helper/update.js
@@ -3,9 +3,9 @@ const uploadedAsAttachment = require('../../lib/uploaded-as-attachment')
 
 module.exports = async function ({ model, params, body, filter, options = {} }) {
   const { _ } = this.ndut.helper
-  const oldData = await dbCall.call(this, { model, method: 'findOne', params: { where: params }, options })
+  const oldData = await dbCall.call(this, { model, method: 'findOne', params: { where: params }, filter, options })
   if (!oldData) throw this.Boom.notFound('recordNotFound', { ndut: 'api' } )
-  await dbCall.call(this, { model, method: 'update', params, body })
+  await dbCall.call(this, { model, method: 'update', params, filter, body })
   const data = await dbCall.call(this, { model, method: 'findOne', params: { where: params }, filter, options })
   if (options.reqId) {
     const modelName = _.isString(model) ? model : model.name
