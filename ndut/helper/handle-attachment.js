c1e40499216c10e42597797aa9ae0e8ac6b09277
Ardhi Lukianto <ardhi@lukianto.com>
2022-08-23 15:32:05 +0700

    Updates

--- ndut/helper/handle-attachment.js
+++ ndut/helper/handle-attachment.js
@@ -1,10 +1,11 @@
 module.exports = async function ({ model, id, column, file }) {
-  const { fs, getConfig } = this.ndut.helper
+  const { fs, getConfig, getNdutConfig } = this.ndut.helper
   const { getModelByAlias } = this.ndutDb.helper
   const config = getConfig()
+  const cfg = getNdutConfig('ndutApi')
+  id = id.replace(/\//g, cfg.slashReplacer)
   const modelName = await getModelByAlias(model)
-  const fname = `${config.dir.data}/attachment/${modelName}/${id}` +
-    `/${column}/${file}`
+  const fname = `${config.dir.data}/attachment/${modelName}/${id}/${column}/${file}`
   if (!fs.existsSync(fname)) throw this.Boom.notFound()
   const stream = fs.createReadStream(fname)
   return { file: fname, stream }
--- ndut/helper/remove.js
+++ ndut/helper/remove.js
@@ -3,8 +3,9 @@ const callBeforeHook = require('../../lib/call-before-hook')
 const callAfterHook = require('../../lib/call-after-hook')
 
 module.exports = async function ({ model, params, filter, options = {} }) {
-  const { _, fs, getConfig } = this.ndut.helper
+  const { _, fs, getConfig, getNdutConfig } = this.ndut.helper
   const config = getConfig()
+  const cfg = getNdutConfig('ndutApi')
   const method = 'remove'
   let oldData = await dbCall.call(this, { model, method: 'findOne', params: { where: params }, options })
   if (!oldData) throw this.Boom.notFound('recordNotFound', { ndut: 'api' })
@@ -13,7 +14,7 @@ module.exports = async function ({ model, params, filter, options = {} }) {
   if (!options.noAfterHook) oldData = await callAfterHook.call(this, { method, model, result: oldData, params, options, filter })
   if (options.reqId) {
     const modelName = _.isString(model) ? model : model.name
-    const dir = `${config.dir.data}/attachment/${modelName}/${oldData.id}`
+    const dir = `${config.dir.data}/attachment/${modelName}/${oldData.id.replace(/\//g, cfg.slashReplacer)}`
     await fs.remove(dir)
   }
   return {
