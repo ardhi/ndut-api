134f46c483b05ee43e856e9636c49b7b2f4331af
Ardhi Lukianto <ardhi@lukianto.com>
2022-11-23 09:41:46 +0700

    Updates

--- ndut/helper/handle-attachment.js
+++ ndut/helper/handle-attachment.js
@@ -1,12 +1,30 @@
-module.exports = async function ({ model, id, column, file }) {
+const path = require('path')
+
+module.exports = async function ({ model, id, column, file, filter }) {
   const { fs, getConfig, getNdutConfig } = this.ndut.helper
   const { getModelByAlias } = this.ndutDb.helper
   const config = getConfig()
   const cfg = getNdutConfig('ndutApi')
   id = id.replace(/\//g, cfg.slashReplacer)
   const modelName = await getModelByAlias(model)
-  const fname = `${config.dir.data}/attachment/${modelName}/${id}/${column}/${file}`
+  const ref = `${modelName}/${id}/${column}/${file}`
+  const fname = `${config.dir.data}/attachment/${ref}`
   if (!fs.existsSync(fname)) throw this.Boom.notFound()
   const stream = fs.createReadStream(fname)
-  return { file: fname, stream }
+  if (cfg.logDownload) {
+    try {
+      const { size } = fs.statSync(fname)
+      const body = {
+        refId: id,
+        model,
+        column,
+        file,
+        type: path.extname(fname).substr(1),
+        size
+      }
+      await this.ndutApi.helper.create({ model: 'ApiLogAtt', body, filter })
+    } catch (err) {}
+  }
+
+  return { file: fname, stream, ref }
 }
